691f3466af8b73d5bfc651660fc629810c4ef369
iliags <19849002+iliags@users.noreply.github.com>
2024-11-13 03:38:36 -0700

    Make note of original QuickBMS script

--- crates/conv_archive/src/lib.rs
+++ crates/conv_archive/src/lib.rs
@@ -1,14 +1,19 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+/* QuickBMS script
+comtype lzss
+endian big
+get asize asize
+do
+getdstring name 13
+get zsize long
+get size long
+savepos offset
+clog name offset zsize size
+math offset += zsize
+goto offset
+while offset < asize
+*/
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    //use super::*;
 }
