186af4448a1709cc54da094ac8acaef9abd72f1e
iliags <19849002+iliags@users.noreply.github.com>
2024-11-13 04:02:15 -0700

    LZSS stub

--- crates/conv_archive/src/lib.rs
+++ crates/conv_archive/src/lib.rs
@@ -1,3 +1,7 @@
+use std::{fs::File, io};
+
+use lzss::{Lzss, SliceReader};
+
 /* QuickBMS script
 comtype lzss
 endian big
@@ -13,7 +17,27 @@ goto offset
 while offset < asize
 */
 
+// TODO: Add a builder pattern implementation which defaults to the QuickBMS script
+
+pub fn extract_archive(input_path: &str, _output_path: &str) -> Result<(), io::Error> {
+    let file = File::open(input_path)?;
+
+    type MyLzss = Lzss<13, 4, 0x20, { 1 << 10 }, { 2 << 10 }>;
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     //use super::*;
+
+    #[test]
+    fn test_lzss() {
+        use lzss::{Lzss, SliceReader, SliceWriter};
+        type MyLzss = Lzss<10, 4, 0x20, { 1 << 10 }, { 2 << 10 }>;
+        let input = b"Example Data";
+        let mut output = [0; 30];
+        let result = MyLzss::compress_stack(SliceReader::new(input), SliceWriter::new(&mut output));
+        assert_eq!(result, Ok(14));
+    }
 }
