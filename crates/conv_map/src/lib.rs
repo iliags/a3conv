75a633d34e8e937fcfcc8f5972974cce8b6661c8
iliags <19849002+iliags@users.noreply.github.com>
2024-12-12 15:22:31 -0700

    Convert map data to CSV

--- crates/conv_map/src/lib.rs
+++ crates/conv_map/src/lib.rs
@@ -1,19 +1,34 @@
-#![allow(dead_code, unused_variables)]
-use face_id::Face;
 use nalgebra::Vector3;
-use std::collections::HashMap;
+use std::ffi::OsStr;
 use std::fs::File;
-use std::io::{BufRead, BufReader, BufWriter, Error, ErrorKind, Write};
-
-pub mod face_id;
+use std::io::{BufRead, BufReader, Error, ErrorKind};
+use std::path::PathBuf;
 
 type Vertex = Vector3<f32>;
 
-#[derive(Debug)]
+#[derive(Debug, Default, Clone)]
+pub struct Map {
+    name: String,
+    vertices: Vec<Vertex>,
+    regions: Vec<Region>,
+    walls: Vec<Wall>,
+}
+
+#[derive(Debug, Default, Clone)]
 pub struct Region {
     name: String,
     floor_height: f32,
     ceiling_height: f32,
+}
+
+impl Region {
+    pub fn floor_height(&self) -> f32 {
+        self.floor_height
+    }
+
+    pub fn ceiling_height(&self) -> f32 {
+        self.ceiling_height
+    }
 }
 
 #[derive(Debug, Default, Clone)]
@@ -42,200 +57,130 @@ enum LineType {
     Comment,
 }
 
-#[derive(Debug)]
-pub struct Graph {
-    vertices: Vec<Vertex>,
-    edges: HashMap<usize, Vec<usize>>, // Adjacency list
-}
+impl Map {
+    pub fn name(&self) -> &String {
+        &self.name
+    }
 
-pub fn parse_wmp(filename: &str) -> Result<(Vec<Vertex>, Vec<Region>, Vec<Wall>), std::io::Error> {
-    //Error::new(std::io::ErrorKind::NotFound, "File not found")
-    //let file = File::open(filename).unwrap_or_else(|_| panic!("Failed to open file: {}", filename));
-    let file = match File::open(filename) {
-        Ok(file) => Ok(file),
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => Err(std::io::Error::new(
-                ErrorKind::NotFound,
-                format!("File not found: {}", filename),
-            )),
-            other_error => Err(Error::new(other_error, "Failed to open file")),
-        },
-    };
+    /// Load a map from a WMP file
+    pub fn parse_wmp(&mut self, filename: &PathBuf) -> Result<(), std::io::Error> {
+        self.name = filename
+            .file_stem()
+            .unwrap_or(OsStr::new("Unknown"))
+            .to_string_lossy()
+            .to_string();
 
-    // The file should be safe to unwrap as errors are handled above
-    let reader = BufReader::new(file.unwrap());
+        //println!("Parsing WMP file: {:?}", self.name);
 
-    let mut vertices = Vec::new();
-    let mut regions = Vec::new();
-    let mut walls = Vec::new();
-
-    for line in reader.lines() {
-        let line = line?;
-        let line = line.trim();
-        let line = line.split_once(';').map_or(line, |(before, _)| before); // Trim everything after ";"
-        let line = line.trim_end();
-
-        if line.is_empty() || line.starts_with('#') {
-            continue; // Skip empty lines and comments
-        }
-
-        let parts: Vec<&str> = line.split_whitespace().collect();
-
-        let line_type = match parts[0] {
-            "VERTEX" => LineType::Vertex,
-            "REGION" => LineType::Region,
-            "WALL" => LineType::Wall,
-            _ => LineType::Comment,
+        let file = match File::open(filename) {
+            Ok(file) => Ok(file),
+            Err(error) => match error.kind() {
+                ErrorKind::NotFound => Err(std::io::Error::new(
+                    ErrorKind::NotFound,
+                    format!("File not found: {:?}", filename.to_str()),
+                )),
+                other_error => Err(Error::new(other_error, "Failed to open file")),
+            },
         };
 
-        match line_type {
-            LineType::Vertex => {
-                // Parse vertex data
-                let x: f32 = parts[1].parse().unwrap_or_default();
-                let y: f32 = parts[2].parse().unwrap_or_default();
-                let z: f32 = parts[3].parse().unwrap_or_default();
-                //vertices.push(Vertex { x, y, z });
-                vertices.push(Vector3::new(x, y, z));
+        // The file should be safe to unwrap as errors are handled above
+        let reader = BufReader::new(file.unwrap());
+
+        let mut vertices = Vec::new();
+        let mut regions = Vec::new();
+        let mut walls = Vec::new();
+
+        for line in reader.lines() {
+            let line = line?;
+            let line = line.trim();
+            let line = line.split_once(';').map_or(line, |(before, _)| before); // Trim everything after ";"
+            let line = line.trim_end();
+
+            if line.is_empty() || line.starts_with('#') {
+                continue; // Skip empty lines and comments
             }
-            LineType::Region => {
-                // Parse region data
-                let name = parts[1].to_string();
-                let floor_hgt: f32 = parts[2].parse().unwrap_or_default();
-                let ceil_hgt: f32 = parts[3].parse().unwrap_or_default();
-                regions.push(Region {
-                    name,
-                    floor_height: floor_hgt,
-                    ceiling_height: ceil_hgt,
-                });
+
+            let parts: Vec<&str> = line.split_whitespace().collect();
+
+            let line_type = match parts[0] {
+                "VERTEX" => LineType::Vertex,
+                "REGION" => LineType::Region,
+                "WALL" => LineType::Wall,
+                _ => LineType::Comment,
+            };
+
+            match line_type {
+                LineType::Vertex => {
+                    // Parse vertex data
+                    let x: f32 = parts[1].parse().unwrap_or_default();
+                    let y: f32 = parts[2].parse().unwrap_or_default();
+                    let z: f32 = parts[3].parse().unwrap_or_default();
+                    //vertices.push(Vertex { x, y, z });
+                    vertices.push(Vector3::new(x, y, z));
+                }
+                LineType::Region => {
+                    // Parse region data
+                    let name = parts[1].to_string();
+                    let floor_hgt: f32 = parts[2].parse().unwrap_or_default();
+                    let ceil_hgt: f32 = parts[3].parse().unwrap_or_default();
+                    regions.push(Region {
+                        name,
+                        floor_height: floor_hgt,
+                        ceiling_height: ceil_hgt,
+                    });
+                }
+                LineType::Wall => {
+                    // Parse wall data
+                    let name = parts[1].to_string();
+                    let vertex1_index: usize = parts[2].parse().unwrap_or_default();
+                    let vertex2_index: usize = parts[3].parse().unwrap_or_default();
+                    let region1_index: usize = parts[4].parse().unwrap_or_default();
+                    let region2_index: usize = parts[5].parse().unwrap_or_default();
+                    let offset_x: f32 = parts[6].parse().unwrap_or_default();
+                    let offset_y: f32 = parts[7].parse().unwrap_or_default();
+                    walls.push(Wall {
+                        name,
+                        vertex1_index,
+                        vertex2_index,
+                        region1_index,
+                        region2_index,
+                        offset_x,
+                        offset_y,
+                        ..Default::default()
+                    });
+                }
+                _ => {}
             }
-            LineType::Wall => {
-                // Parse wall data
-                let name = parts[1].to_string();
-                let vertex1_index: usize = parts[2].parse().unwrap_or_default();
-                let vertex2_index: usize = parts[3].parse().unwrap_or_default();
-                let region1_index: usize = parts[4].parse().unwrap_or_default();
-                let region2_index: usize = parts[5].parse().unwrap_or_default();
-                let offset_x: f32 = parts[6].parse().unwrap_or_default();
-                let offset_y: f32 = parts[7].parse().unwrap_or_default();
-                walls.push(Wall {
-                    name,
-                    vertex1_index,
-                    vertex2_index,
-                    region1_index,
-                    region2_index,
-                    offset_x,
-                    offset_y,
-                    ..Default::default()
-                });
-            }
-            _ => {}
         }
+
+        self.vertices = vertices;
+        self.regions = regions;
+        self.walls = walls;
+
+        Ok(())
     }
 
-    Ok((vertices, regions, walls))
-}
+    /// Create a list of vertices from the map data
+    pub fn create_vertex_list(&self) -> Vec<String> {
+        let mut output_data: Vec<String> = Vec::new();
+        for (vertex, region) in self.vertices.iter().zip(self.regions.iter()) {
+            let vert1 = format!("{},{},{}", vertex.x, vertex.y, region.floor_height());
+            let vert2 = format!("{},{},{}", vertex.x, vertex.y, region.ceiling_height());
 
-pub fn build_graph(vertices: Vec<Vertex>, walls: Vec<Wall>) -> Graph {
-    let mut graph = Graph {
-        vertices,
-        edges: HashMap::new(),
-    };
-
-    for wall in walls {
-        graph
-            .edges
-            .entry(wall.vertex1_index)
-            .or_default()
-            .push(wall.vertex2_index);
-        graph
-            .edges
-            .entry(wall.vertex2_index)
-            .or_default()
-            .push(wall.vertex1_index);
-    }
-
-    graph
-}
-
-pub fn write_output(
-    faces: Vec<Face>,
-    normals: Vec<Vector3<f32>>,
-    output_file: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
-    // ... implementation
-    Ok(())
-}
-
-pub fn write_to_obj(
-    faces: &[Face],
-    vertices: &[Vertex],
-    normals: &[Vector3<f32>],
-    output_file: &str,
-) -> Result<(), std::io::Error> {
-    let file = File::create(output_file)?;
-    let mut writer = BufWriter::new(file);
-
-    // Write vertices
-    for (i, vertex) in vertices.iter().enumerate() {
-        write!(writer, "v {} {} {}\n", vertex.x, vertex.y, vertex.z)?;
-    }
-
-    // Write normals
-    for (i, normal) in normals.iter().enumerate() {
-        write!(writer, "vn {} {} {}\n", normal.x, normal.y, normal.z)?;
-    }
-
-    // Write faces
-    for face in faces {
-        write!(writer, "f")?;
-        for (i, vertex_index) in face.outer_loop.iter().enumerate() {
-            write!(writer, " {}//{}", vertex_index + 1, i + 1)?;
+            output_data.push(vert1);
+            output_data.push(vert2);
         }
-        writeln!(writer)?;
+
+        output_data
     }
 
-    Ok(())
-}
+    /// Creates a CSV string from the vertex data
+    pub fn create_vertex_csv(&self) -> String {
+        let mut output = Vec::new();
 
-/* Old code from main.rs
-struct Args {
-    /// Name of the map (*.wmp) to convert
-    #[arg(short, long)]
-    map: String,
+        output.push("x,y,z".to_string());
+        output.append(&mut self.create_vertex_list());
 
-    /// Name of the output file
-    #[arg(short, long)]
-    output: String,
-}
-
-let (vertices, regions, walls) = match parse_wmp(&args.map) {
-    Ok((vertices, regions, walls)) => (vertices, regions, walls),
-    Err(error) => {
-        eprintln!("Failed to parse map: {}", error);
-        return;
+        output.join("\n")
     }
-};
-
-//println!("Vertices: {:?}", vertices);
-
-let graph = build_graph(vertices.clone(), walls.clone());
-//println!("Graph: {:?}", graph);
-
-let faces = identify_faces(&graph);
-//println!("Faces: {:?}", faces);
-
-let face_normals = calculate_face_normals(faces.clone(), &vertices);
-//println!("Face normals: {:?}", face_normals);
-
-//if let Err(error) = write_output(faces.clone(), face_normals, &args.output) {
-//    eprintln!("Failed to write output: {}", error);
-//}
-
-println!("Vertices: {:?}", vertices.len());
-println!("Regions: {:?}", regions.len());
-println!("Walls: {:?}", walls.len());
-
-if let Err(error) = write_to_obj(&faces, &vertices, &face_normals, &args.output) {
-    eprintln!("Failed to write output: {}", error);
 }
-*/
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::path::Path;
-
 use clap::Parser;
+use std::{
+    fs::{self},
+    path::{Path, PathBuf},
+};
 
 // TODO: Subcommands for each step of the conversion process
 #[derive(Parser, Debug)]
@@ -18,24 +20,42 @@ struct Args {
 const DEBUG_FILE: &str = "target/debug/apathy.wrs";
 const DEBUG_OUTPUT: &str = "target/debug/apathy";
 
+const DEBUG_MAP: &str = "target/debug/apathy.WMP";
+
 fn main() {
     let args = Args::parse();
 
-    let archive = args.archive.unwrap_or(DEBUG_FILE.to_string());
+    let _archive = args.archive.unwrap_or(DEBUG_FILE.to_string());
     let output = args.output.unwrap_or(DEBUG_OUTPUT.to_string());
-
-    println!("Extracting archive: {}", archive);
 
     // Create the output directory if it doesn't exist
     if !Path::new(&output).exists() {
         std::fs::create_dir(&output).unwrap();
     }
 
+    /*
+    println!("Extracting archive: {}", archive);
+
     // Extract the archive
     match conv_archive::extract_archive(&archive, &output) {
         Ok(_) => println!("Extraction complete!"),
         Err(e) => eprintln!("Error: {}", e),
     }
+     */
 
     //println!("Conversion complete!");
+
+    let mut map = conv_map::Map::default();
+
+    let path = PathBuf::from(DEBUG_MAP);
+    match map.parse_wmp(&path) {
+        Ok(_) => {
+            let output_file = format!("{}/{}.txt", output, map.name());
+
+            println!("Writing to file: {:?}", output_file);
+
+            fs::write(output_file, map.create_vertex_csv()).unwrap();
+        }
+        Err(e) => eprintln!("Error: {}", e),
+    }
 }
